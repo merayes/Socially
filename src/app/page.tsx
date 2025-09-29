915b770bbbcfa162ec05531fff776e378fea97dd
merayes <02200201039@ogr.inonu.edu.tr>
2025-09-29 20:58:25 +0300

    prisma updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="m-4">
