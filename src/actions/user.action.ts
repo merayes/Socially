7130a9b501392e1754d4529890074ab3cd9a4578
merayes <02200201039@ogr.inonu.edu.tr>
2025-09-30 12:52:15 +0300

    updated code

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -51,3 +51,11 @@ export async function getUserByClerkId(clerkId: string) {
     },
   });
 }
+
+export async function getDbUserId() {
+  const { userId: clerckId } = await auth();
+  if (!clerckId) throw new Error("Not authenticated");
+  const user = await getUserByClerkId(clerckId);
+  if (!user) throw new Error("User not found in db");
+  return user.id;
+}
